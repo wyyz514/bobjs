b2e91ac30fa963db37e54568cb8192d0e51d72b2
Oluwole Obayomi <oobayomi@G000587.local>
2020-08-20 23:46:46 -0400

    using deep replacement for array index in template

--- src/LumberOperators/Each/index.js
+++ src/LumberOperators/Each/index.js
@@ -4,11 +4,7 @@ export default function each({ lumberObj, data }) {
     if (Array.isArray(data)) {
       for (let i = 0; i < data.length; i++) {
         for (const child of lumberObj.children) {
-          const childWithIndex = { ...child };
-          childWithIndex.text = childWithIndex.text.replace(
-            /\(index\)/g,
-            `${i}`
-          );
+          const childWithIndex = replaceIndex({ ...child }, i);
           spreadTemplates.push(childWithIndex);
         }
       }
@@ -19,3 +15,17 @@ export default function each({ lumberObj, data }) {
     each({ lumberObj: child, data });
   }
 }
+
+function replaceIndex(lumberObj, i) {
+  if (lumberObj == null) {
+    return;
+  }
+  const withIndex = { ...lumberObj, children: [] };
+  withIndex.text = withIndex.text.replace(/\(index\)/g, `${i}`);
+  for (const child of lumberObj.children) {
+    const childWithIndex = replaceIndex({ ...child }, i);
+    withIndex.children.push(childWithIndex);
+  }
+  return withIndex;
+}
+
