f93deeaebaf3469f898aeb4578568368b4f8e276
Oluwole Obayomi <oobayomi@G000587.local>
2020-08-20 23:47:58 -0400

    pulling all templates into single example file rendered throught index.js

--- src/index.js
+++ src/index.js
@@ -1,95 +1,89 @@
-import HelloBob from "./Examples/hellobob";
-import Tools from "./Examples/lists";
-import ShowDrills from "./Examples/conditional";
-import RecursiveMenu, { MenuTitle } from "./Examples/recursive";
+import Examples from "./Examples";
 
-import "./styles.css";
 
 const appMount = document.getElementById("app");
 
-const helloBob = new HelloBob({ name: "Bob" });
-
-const tools = new Tools([
-  {
-    name: "Drill",
-    description: "To drill stuff"
+const data = {
+  bobData: {
+    name: "Bob"
   },
-  {
-    name: "Saw",
-    description: "To saw stuff"
-  }
-]);
+  tools: [
+    {
+      name: "Drill",
+      description: "To drill stuff"
+    },
+    {
+      name: "Saw",
+      description: "To saw stuff"
+    }
+  ],
+  toFilter: [
+    {
+      type: "drill",
+      brand: "Bosch"
+    },
+    {
+      type: "saw",
+      brand: "Bosch"
+    },
+    {
+      type: "drill",
+      brand: "Black & Decker"
+    }
+  ],
+  menu: [
+    {
+      nodeName: "Node 1",
+      nodes: [
+        {
+          nodeName: "Node 1.1",
+          nodes: []
+        },
+        {
+          nodeName: "Node 1.2",
+          nodes: [
+            {
+              nodeName: "Node 1.2.1",
+              nodes: []
+            },
+            {
+              nodeName: "Node 1.2.2",
+              nodes: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      nodeName: "Node 2",
+      nodes: [
+        {
+          nodeName: "Node 2.1",
+          nodes: [
+            {
+              nodeName: "Node 2.1.1",
+              nodes: [
+                {
+                  nodeName: "Node 2.1.1.1",
+                  nodes: []
+                },
+                {
+                  nodeName: "Node 2.1.1.2",
+                  nodes: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          nodeName: "Node 2.2",
+          nodes: []
+        }
+      ]
+    }
+  ]
+};
 
-const showOnlyDrills = new ShowDrills([
-  {
-    type: "drill",
-    brand: "Bosch"
-  },
-  {
-    type: "saw",
-    brand: "Bosch"
-  },
-  {
-    type: "drill",
-    brand: "Black & Decker"
-  }
-]);
-
-const recursiveMenu = new RecursiveMenu([
-  {
-    nodeName: "Node 1",
-    nodes: [
-      {
-        nodeName: "Node 1.1",
-        nodes: []
-      },
-      {
-        nodeName: "Node 1.2",
-        nodes: [
-          {
-            nodeName: "Node 1.2.1",
-            nodes: []
-          },
-          {
-            nodeName: "Node 1.2.2",
-            nodes: []
-          }
-        ]
-      }
-    ]
-  },
-  {
-    nodeName: "Node 2",
-    nodes: [
-      {
-        nodeName: "Node 2.1",
-        nodes: [
-          {
-            nodeName: "Node 2.1.1",
-            nodes: [
-              {
-                nodeName: "Node 2.1.1.1",
-                nodes: []
-              },
-              {
-                nodeName: "Node 2.1.1.2",
-                nodes: []
-              }
-            ]
-          }
-        ]
-      },
-      {
-        nodeName: "Node 2.2",
-        nodes: []
-      }
-    ]
-  }
-]);
-
-appMount.innerHTML = helloBob.build().render();
-appMount.innerHTML += tools.build().render();
-appMount.innerHTML += showOnlyDrills.build().render();
-appMount.innerHTML += new MenuTitle().build().render();
-appMount.innerHTML += recursiveMenu.build().render();
+var examples = new Examples(data);
+appMount.innerHTML = examples.build().render();
 
